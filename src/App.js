ad65eed7f6cf317823374de4cb0ecf6d36baa0c7
hajczek <i.hajczewska@gmail.com>
2020-08-27 12:55:24 +0200

    Added import for css and js of materialize

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
