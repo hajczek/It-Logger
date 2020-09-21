aa907d4b71e9de1e5438016e7f736c8cbfe71d83
hajczek <i.hajczewska@gmail.com>
2020-09-21 16:42:22 +0200

    Fixed mistakes of class and for atributes

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -3,14 +3,14 @@ import React from 'react';
 const SearchBar = () => {
   return (
     <nav style={{ marginBottom: '30px' }} className='blue'>
-      <div class='nav-wrapper'>
+      <div className='nav-wrapper'>
         <form>
-          <div class='input-field'>
+          <div className='input-field'>
             <input id='search' type='search' required />
-            <label class='label-icon' for='search'>
-              <i class='material-icons'>search</i>
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
             </label>
-            <i class='material-icons'>close</i>
+            <i className='material-icons'>close</i>
           </div>
         </form>
       </div>
