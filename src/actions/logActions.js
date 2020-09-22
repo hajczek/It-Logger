50634d459ee7b6fa719e5d7e6c0ecdf7a31f3bca
hajczek <i.hajczewska@gmail.com>
2020-09-22 18:45:06 +0200

    Added functionality for getLogs function

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,6 +1,23 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 
-export const getLogs = () => {};
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
 
 // Set loading to true
 export const setLoading = () => {
