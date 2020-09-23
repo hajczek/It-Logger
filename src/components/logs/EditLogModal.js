7eb860d4d49b17f84e2b8d7cc7a83a4e7265a0a0
hajczek <i.hajczewska@gmail.com>
2020-09-23 18:32:47 +0200

    Added updLog to onSubmit function

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -21,7 +21,16 @@ const EditLogModal = ({ current, updateLog }) => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(message, tech, attention);
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date(),
+      };
+
+      updateLog(updLog);
+      M.toast({ html: `Log updated by ${tech}` });
 
       // Clear Fields
       setMessage('');
