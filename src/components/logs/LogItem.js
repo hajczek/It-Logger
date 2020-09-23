52033f1818e5171fda0a29798abc1e479fb4c2af
hajczek <i.hajczewska@gmail.com>
2020-09-23 17:35:24 +0200

    Added function deleteLog to LogItem component

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { deleteLog } from '../../actions/logActions';
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog }) => {
   return (
     <li className='collection-item'>
       <div>
@@ -30,6 +32,7 @@ const LogItem = ({ log }) => {
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(null, {deleteLog})(LogItem;)
