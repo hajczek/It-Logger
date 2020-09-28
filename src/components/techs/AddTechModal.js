8e0a1138e77e3cb3e5caffab53efe6a61f8e7405
hajczek <i.hajczewska@gmail.com>
2020-09-28 16:38:06 +0200

    Fixed typo

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
   const onSubmit = () => {
     if (firstName === '' || lastName === '') {
-      M.toast({ html: 'Please enter th first and last name' });
+      M.toast({ html: 'Please enter the first and last name' });
     } else {
-      console.log(firstName, lastName);
+      addTech({
+        firstName,
+        lastName,
+      });
 
       // Clear Fields
       setFirstName('');
@@ -61,4 +67,8 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
