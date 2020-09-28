2246606079438d75c6411470f1cf8445faef5e4d
hajczek <i.hajczewska@gmail.com>
2020-09-28 09:56:01 +0200

    Added searchLogs action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,7 @@ import {
   LOGS_ERROR,
   ADD_LOG,
   DELETE_LOG,
+  SEARCH_LOGS,
   UPDATE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
@@ -93,6 +94,26 @@ export const updateLog = (log) => async (dispatch) => {
 
     dispatch({
       type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Search server logs
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data,
     });
   } catch (err) {
